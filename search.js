e05e2dd3c588b57e9bc79c4a97c760af685971ea
farhanthegreat <farhantuber@gmail.com>
2023-02-02 23:48:45 +0600

    added

---
+++ search.js
@@ -0,0 +1,7 @@
+const lyrics = 'tumi bondhu kala pakhi, ami jeno ki. bosonto kale tomay bhulte pari ni. kala kala sada sada';
+
+
+const searchString = 'Pakhi';
+const doesExist = lyrics.includes(searchString);
+console.log(doesExist);
+const lyricsLowerCase = lyrics.toLowerCase();
